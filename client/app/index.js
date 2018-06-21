4746c228c2d676ea986beda158b749faaf5ac9c9
sullyvan nunes <sullyvan.nunes@yahoo.com.br>
2018-06-21 00:10:45 -0300

    adds transaction in list

--- client/app/index.js
+++ client/app/index.js
@@ -6,6 +6,26 @@ var campos = [
 
 var tbody = document.querySelector('table tbody');
 
-document.querySelector('.form').addEventListener('submit', function() {
-    alert('oi')
-});
+document.querySelector('.form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var tr = document.createElement('tr');
+
+    campos.forEach(function(campo) {
+        var td = document.createElement('td');
+        td.textContent = campo.value;
+        tr.appendChild(td);
+    });
+
+    var tdVolume = document.createElement('td');
+    tdVolume.textContent = campos[1].value * campos[2].value;
+
+    tr.appendChild(tdVolume);
+
+    tbody.appendChild(tr);
+
+    campos[0].value = '';
+    campos[1].value = '';
+    campos[2].value = '';
+
+    campos[0].focus;
+}); 
