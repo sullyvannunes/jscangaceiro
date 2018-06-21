f18a8d523a9e7f48d2d456e6a2849edad217daca
sullyvan nunes <sullyvan.nunes@yahoo.com.br>
2018-06-20 23:36:09 -0300

    adds submit event listener

---
+++ client/app/index.js
@@ -0,0 +1,11 @@
+var campos = [
+    document.querySelector('#data'),
+    document.querySelector('#valor'),
+    document.querySelector('#quantidade')
+];
+
+var tbody = document.querySelector('table tbody');
+
+document.querySelector('.form').addEventListener('submit', function() {
+    alert('oi')
+});
